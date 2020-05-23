b506f0320f5689ca2eb232770bad76fc98ef4a98
Doran Kayoumi <dorankayoumi@gmail.com>
2020-05-23 13:48:42 +0200

    Parse geojson file and convert it to country array

--- src/main/java/ch/heigvd/ser/labo/GeoJsonToKml.java
+++ src/main/java/ch/heigvd/ser/labo/GeoJsonToKml.java
@@ -2,8 +2,9 @@ package ch.heigvd.ser.labo;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 
-public class JsonToKml {
+public class GeoJsonToKml {
 
     final static String GEO_JSON = "src/main/resources/countries.geojson";
 
@@ -13,12 +14,10 @@ public class JsonToKml {
             FileReader reader = new FileReader(GEO_JSON);
 
             GeoParser parser = new GeoParser(reader);
+            ArrayList<Country> countries = parser.parse();
 
-            System.out.println(reader);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-        System.out.println("Hello SER");
     }
 }
--- src/main/java/ch/heigvd/ser/labo/GeoParser.java
+++ src/main/java/ch/heigvd/ser/labo/GeoParser.java
@@ -1,4 +1,51 @@
 package ch.heigvd.ser.labo;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+
 public class GeoParser {
+    private Reader file;
+
+    public GeoParser(Reader data) {
+        this.file = data;
+    }
+
+    public ArrayList<Country> parse() {
+        ArrayList<Country> result = new ArrayList<>();
+
+        try {
+            JSONParser parser = new JSONParser();
+            // get the contents of the GeoJson file as a JSON array
+            JSONObject contents = (JSONObject) parser.parse(file);
+
+            JSONArray countries = (JSONArray) contents.get("features");
+            for (Object c : countries) {
+                JSONObject geoInfo = (JSONObject) c;
+                JSONObject properties = (JSONObject) geoInfo.get("properties");
+                JSONObject geometry = (JSONObject) geoInfo.get("geometry");
+
+                Country country = new Country(
+                    properties.get("ADMIN").toString(),
+                    properties.get("ISO_A3").toString(),
+                    geometry.get("type").toString(),
+                    (JSONArray) geometry.get("coordinates")
+                );
+
+                System.out.println(country);
+
+                result.add(country);
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
 }
